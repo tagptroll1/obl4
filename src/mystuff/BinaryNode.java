121d52eb9dedd97dfe734280d5d12b529b080508
waaaz <hkmelaa@hotmail.com>
2018-05-16 18:36:24 +0200

    i fiks mor

---
+++ src/mystuff/BinaryNode.java
@@ -0,0 +1,63 @@
+package mystuff;
+import oblig_filer.Entry;
+
+public class BinaryNode{
+
+    private BinaryNode left, right;
+
+    private Entry data;
+
+
+    BinaryNode(Entry data){
+        this(null,null,data);
+    }
+
+    BinaryNode(BinaryNode left, BinaryNode right , Entry data){
+        this.data = data;
+
+        this.left = left;
+        this.right = right;
+    }
+
+    public BinaryNode getLeft() {
+        return left;
+    }
+
+    public BinaryNode getRight() {
+        return right;
+    }
+
+    public void setLeft(BinaryNode left) {
+        this.left = left;
+    }
+
+    public void setRight(BinaryNode right) {
+        this.right = right;
+    }
+
+
+    public Entry getData() {
+        return data;
+    }
+
+    public void setData(Entry data) {
+        this.data = data;
+    }
+
+    /**
+     * @return TRUE if node left child is not null.
+     */
+    public boolean hasLeft(){
+        return this.left != null;
+    }
+    /**
+     * @return TRUE if node right child is not null.
+     */
+    public boolean hasRight(){
+        return this.right != null;
+    }
+    public boolean isLeaf(){
+        return (left != null) && (right != null);
+    }
+
+}
--- src/mystuff/SortedTreeMap.java
+++ src/mystuff/SortedTreeMap.java
@@ -10,259 +10,122 @@ import java.util.function.BiPredicate;
 
 public class SortedTreeMap<K extends Comparable<? super K>, V> implements ISortedTreeMap<K, V>{
     private int numberOfEntries;
-
     private BinaryNode rootNode;
 
-    //------------------------
+    private Comparator<K> comp;
 
-    private class BinaryNode{
-        private BinaryNode parent, left, right;
-        private Entry data;
-
-        //"rut"
-        BinaryNode(){
-            this(null,null,null,null);
-        }
-
-        BinaryNode(Entry data){
-            this(null,null,null,data);
-        }
-
-        BinaryNode(BinaryNode parent, BinaryNode left, BinaryNode right , Entry data){
-            this.data = data;
-
-            this.parent = parent;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-
-    //------------------------
 
     public SortedTreeMap(Comparator<K> kComparator) {
-
+        numberOfEntries = 0;
+        rootNode = null;
     }
 
-    /**
-     * Finds the minimum value in the map, if no value is found, returns null instead.
-     *
-     * @return minimum value
-     */
+
+
     @Override
     public Entry<K, V> min() {
         return null;
     }
 
-    /**
-     * Finds the maximum value in the map, if no value is found returns null instead.
-     *
-     * @return maximum value
-     */
+
     @Override
     public Entry<K, V> max() {
         return null;
     }
 
-    /**
-     * Inserts the specified value with the specified key as a new entry into the map.
-     * If the value is already present, return the previous value, else null.
-     *
-     * @param key   The key to be inserted
-     * @param value The value to be inserted
-     * @return Previous value
-     */
+    // TODO: 16.05.2018  ...if root == null -> new map/ rootnode... 
     @Override
     public V add(K key, V value) {
         return null;
     }
-
-    /**
-     * Inserts the specified entry into the map. If the key is already a part of the map,
-     * return the previous value, else null.
-     *
-     * @param entry The new entry to be inserted into the map
-     * @return Previous value
-     */
+    
     @Override
     public V add(Entry<K, V> entry) {
         return null;
     }
 
-    /**
-     * Replaces the value for key in the map as long as it is already present. If they key
-     * is not present, the method throws an exception.
-     *
-     * @param key   The key for which the value is replaced
-     * @param value The new value
-     * @throws NoSuchElementException When key is not in map
-     */
+
     @Override
     public void replace(K key, V value) throws NoSuchElementException {
 
     }
-
-    /**
-     * Applies a function to the value at key and replaces that value. Throws an exception
-     * if the key is not present in the map.
-     *
-     * @param key The key for which we are replacing the value
-     * @param f   The function to apply to the value
-     * @throws NoSuchElementException When key is not in map
-     */
+    
     @Override
     public void replace(K key, BiFunction<K, V, V> f) throws NoSuchElementException {
 
     }
 
-    /**
-     * Removes the entry for key in the map. Throws an exception if the key is not present
-     * in the map.
-     *
-     * @param key The key for the entry to remove
-     * @return The removed value
-     * @throws NoSuchElementException When key is not in map.
-     */
+
     @Override
     public V remove(Object key) throws NoSuchElementException {
         return null;
     }
-
-    /**
-     * Retrieves the value for the key in the map.
-     *
-     * @param key The key for the value to retrieve
-     * @return The value for the key
-     * @throws NoSuchElementException When key is not in map
-     */
+    
     @Override
     public V getValue(Object key) throws NoSuchElementException {
         return null;
     }
-
-    /**
-     * Checks if a key is in the map.
-     *
-     * @param key The key to check
-     * @return true if the key is in the map, false otherwise
-     */
+    
     @Override
     public boolean containsKey(K key) {
         return false;
     }
-
-    /**
-     * Checks if a value is in the map
-     *
-     * @param value the value to look for
-     * @return True if the value is present, false otherwise
-     */
+    
     @Override
     public boolean containsValue(V value) {
         return false;
     }
 
-    /**
-     * Finds all the keys in the map and returns them in order.
-     *
-     * @return keys in order
-     */
+
     @Override
     public Iterable<K> keys() {
         return null;
     }
-
-    /**
-     * Finds the values in order of the keys.
-     *
-     * @return values in order of the keys
-     */
+    
     @Override
     public Iterable<V> values() {
         return null;
     }
 
-    /**
-     * Finds all entries in the map in order of the keys.
-     *
-     * @return All entries in order of the keys
-     */
+
     @Override
     public Iterable<Entry<K, V>> entries() {
         return null;
     }
-
-    /**
-     * Finds the entry for the key, if the key is not in the map returns the next
-     * highest entry if such an entry exists
-     *
-     * @param key The key to find
-     * @return The entry for the key or the next highest
-     */
+    
     @Override
     public Entry<K, V> higherOrEqualEntry(K key) {
         return null;
     }
-
-    /**
-     * Finds the entry for the key, if the key is not in the map, returns the next
-     * lower entry if such an entry exists
-     *
-     * @param key The key to find
-     * @return The entry for the key or the next lower
-     */
+    
     @Override
     public Entry<K, V> lowerOrEqualEntry(K key) {
         return null;
     }
-
-    /**
-     * Adds all entries in the other map into the current map. If a key is present
-     * in both maps, the key in the other map takes precedent.
-     *
-     * @param other The map to add to the current map.
-     */
+    
     @Override
     public void merge(ISortedTreeMap<K, V> other) {
 
     }
-
-    /**
-     * Removes any entry for which the predicate holds true. The predicate can
-     * trigger on both the key and value of each entry.
-     *
-     * @param p The predicate that tests which entries should be kept.
-     */
+    
     @Override
     public void removeIf(BiPredicate<K, V> p) {
 
     }
-
-    /**
-     * Checks if the map is empty
-     *
-     * @return True if the map is empty, false otherwise.
-     */
+    
     @Override
     public boolean isEmpty() {
         return size() <= 0;
     }
-
-    /**
-     * Returns the number of entries in the map
-     *
-     * @return Number of entries
-     */
+    
     @Override
     public int size() {
         return numberOfEntries;
     }
-
-    /**
-     * Clears the map of entries.
-     */
+    
     @Override
     public void clear() {
-
+        numberOfEntries = 0;
+        rootNode = null;
     }
 }
